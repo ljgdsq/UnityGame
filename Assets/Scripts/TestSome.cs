6cdb203954732db1c4c48b4f0732c73c0dbde2dd
zhengshulin <zhengshulin@zwwxgame.com>
2022-10-13 14:42:28 +0800

    add rx

--- Assets/Scripts/TestSome.cs
+++ Assets/Scripts/TestSome.cs
@@ -1,4 +1,7 @@
+using System;
+using UniRx;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TestSome :  MonoBehaviour
 {
@@ -7,6 +10,30 @@ public class TestSome :  MonoBehaviour
     {
         Debug.developerConsoleVisible = false;
         _isStart = true;
+
+        Observable.IntervalFrame(60).TakeUntilDisable(this).Subscribe(x => Debug.Log(x), () =>
+        {
+            Debug.Log("complete!");
+            
+        });
+        
+        Observable.EveryUpdate().Where(_ => Input.GetMouseButtonDown(0)).Select((t1, t2) =>
+        {
+            
+            Debug.Log(t1);
+            Debug.Log(t2);
+            return t1+t2;
+        }).Subscribe((t) =>
+        {
+            Debug.Log(t);
+        });
+       var leftEvent= Observable.EveryUpdate().Where(_ => Input.GetMouseButtonDown(0)).Select(_ => true);
+        var rightEvent=Observable.EveryUpdate().Where(_ => Input.GetMouseButtonUp(0)).Select(_ => false);
+        var mouseUp = Observable.Merge(leftEvent,rightEvent);
+        var reactiveCommand = new ReactiveCommand(mouseUp,false);
+        reactiveCommand.Subscribe(x => { Debug.Log(x); });
+//        Observable.EveryUpdate().Subscribe(t => { reactiveCommand.Execute(); }).AddTo(this);
+
     }
 
 
