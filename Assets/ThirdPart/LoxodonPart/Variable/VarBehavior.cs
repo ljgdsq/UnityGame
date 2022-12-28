9141ca9624cba39dae4ee789721bd236422c33c1
zhengshulin <zhengshulin@zwwxgame.com>
2022-12-28 11:38:18 +0800

    add a useful component

---
+++ Assets/ThirdPart/LoxodonPart/Variable/VarBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Loxodon.Framework.Views.Variables;
+using UnityEngine;
+
+public class VarBehavior : MonoBehaviour
+{
+
+    public VariableArray VariableArray;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
