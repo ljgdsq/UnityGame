8bcdeccfbc82709a11555548232110428bad53c5
ljgdsq <742706540@qq.com>
2023-05-19 18:27:00 +0800

    update

--- Assets/Editor/AndroidGradleProcess.cs
+++ Assets/Editor/AndroidGradleProcess.cs
@@ -10,9 +10,9 @@ public class AndroidGradleProcess : IPostGenerateGradleAndroidProject
     public int callbackOrder { get; }
     public void OnPostGenerateGradleAndroidProject(string path)
     {
-//        var filePath = Path.Combine(Application.dataPath,"Plugins/Android/a.gradle");
-//        var desPath = Path.Combine(path, "a.gradle");
-//        Debug.LogError($"copy from {filePath} to {desPath}");
+        var filePath = Path.Combine(Application.dataPath,"Plugins/Android/a.gradle");
+        var desPath = Path.Combine(path, "a.gradle");
+        Debug.LogError($"copy from {filePath} to {desPath}");
 //        File.Copy(filePath,desPath);
         
     }
