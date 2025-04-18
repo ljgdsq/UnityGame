a61881ff55959694485e532d86f855eb709bec71
ljgdsq <31955038+ljgdsq@users.noreply.github.com>
2025-04-18 23:13:26 +0800

    update

--- Assets/NewFramework/UIFramework/Example/TestDialogView.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using Goose.UIFramework;
-using UnityEngine;
-
-[UIConfig("TestViews/TestDialog", UIType.Dialog, UISize.Custom)]
-public class TestDialogView : UIBase { }
--- Assets/NewFramework/UIFramework/Example/TestPersistentView.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using Goose.UIFramework;
-using UnityEngine;
-
-
-[UIConfig("TestViews/PersistentView", UIType.Window, UISize.Custom, UILayer.Dialog,isPersistent: true)]
-public class TestPersistentView : UIBase { }
