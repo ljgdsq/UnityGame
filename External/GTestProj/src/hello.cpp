f4ff4e0b7134579d8fb6647673879f941358f61e
ljgdsq <742706540@qq.com>
2025-08-28 17:23:00 +0800

    modern cmake project template

--- External/GTestProj/src/hello.cpp
+++ External/GTestProj/src/hello.cpp
@@ -1,6 +1,8 @@
+#include <spdlog/spdlog.h>
 #include <iostream>
 
 int main() {
+    spdlog::info("Hello from spdlog!");
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
--- External/GTestProj/tests/test_hello.cpp
+++ External/GTestProj/tests/test_hello.cpp
@@ -1,7 +1,13 @@
 #include <gtest/gtest.h>
+#include <spdlog/spdlog.h>
 
 TEST(HelloTest, Basic) {
     EXPECT_EQ(1 + 1, 2);
+}
+
+TEST(SpdlogTest, LogInfo) {
+    spdlog::set_level(spdlog::level::info);
+    EXPECT_NO_THROW(spdlog::info("Test info log"));
 }
 
 int main(int argc, char **argv) {
